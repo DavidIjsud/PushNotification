31bd463bcb5ee36a150fb7dab7391614974447db
David Garcia <david.garcia@oktana.com>
2021-08-17 23:05:41 -0400

    Saving all

--- lib/pages/mensaje_page.dart
+++ lib/pages/mensaje_page.dart
@@ -11,7 +11,7 @@ class MensajePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mensaje de notificación 2 mejorado'),
+        title: Text('Mensaje de notificación 2 mejorado otra vez'),
       ),
       body: Center(
         child: Text(arg),
